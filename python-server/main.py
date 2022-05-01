56fbf4aee2d17ff0c0258947adf7217be65f3fa0
Brixter Porras <brixterporras@gmail.com>
2022-05-01 16:37:16 +0800

    refactor: ParkSlot to Location

--- python-server/main.py
+++ python-server/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from config.database import engine,Base
 from fastapi.staticfiles import StaticFiles
 
-from routes import image_routes,park_slot_routes
+from routes import image_routes,location_routes
 
 
 Base.metadata.create_all(engine)#Initialize Database
@@ -11,7 +11,7 @@ app = FastAPI(title="Parking Lot MS")
 app.mount("/data", StaticFiles(directory="../data"), name="static")
 
 app.include_router(image_routes.router)
-app.include_router(park_slot_routes.router)
+app.include_router(location_routes.router)
 
 @app.get("/")
 async def root():
