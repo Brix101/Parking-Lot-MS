e7359d8dd5470533bfb34c0c4d26cd0893b0f754
Brixter Porras <brixterporras@gmail.com>
2022-05-01 16:43:21 +0800

    feat: Added Parker Routes

--- python-server/main.py
+++ python-server/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from config.database import engine,Base
 from fastapi.staticfiles import StaticFiles
 
-from routes import image_routes,location_routes
+from routes import image_routes,location_routes,parker_routes
 
 
 Base.metadata.create_all(engine)#Initialize Database
@@ -12,7 +12,8 @@ app.mount("/data", StaticFiles(directory="../data"), name="static")
 
 app.include_router(image_routes.router)
 app.include_router(location_routes.router)
+app.include_router(parker_routes.router)
 
 @app.get("/")
 async def root():
-    return {"detail": "Hello from PLMS"}
+    return {"message": "☁☁☁"}
