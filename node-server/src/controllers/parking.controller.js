f2d37434922a38a638add470ae2afc8559496f13
Brixter Porras <brixterporras@gmail.com>
2022-05-26 00:00:48 +0800

    fix: Get parking logs by desc

--- node-server/src/controllers/parking.controller.js
+++ node-server/src/controllers/parking.controller.js
@@ -5,7 +5,7 @@ const sequilize = require("../utils/database");
 const getAllController = async (req, res) => {
   try {
     data = await sequilize.query(
-      "SELECT Parkings.id AS ParkingId,Parkers.id AS ParkerID,Parkings.entered,Parkings.exited,Parkers.plateNumber,Parkers.note FROM Parkers INNER JOIN Parkings ON Parkings.parkerId = Parkers.id",
+      "SELECT Parkings.id AS ParkingId,Parkers.id AS ParkerID,Parkings.entered,Parkings.exited,Parkers.plateNumber,Parkers.note FROM Parkers INNER JOIN Parkings ON Parkings.parkerId = Parkers.id ORDER BY Parkings.entered DESC",
       {
         type: QueryTypes.SELECT,
       }
